27329724ca62bd53151a71bf20af36988c25ec53
Arjun <arjun.narain700@gmail.com>
2021-01-26 12:23:17 -0600

    1stcssupdate

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = props => {
   return (
-    <Route {...props}>
+    <Route {...props} >
       {(props.authenticated)? props.children  : <Redirect to="/login" />}
     </Route>
   )
