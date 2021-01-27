f66cc548b3ac2d942133a97b20cf2d2861670b37
Thanh Nguyen <thanhnguyenttn@gmail.com>
2021-01-26 19:41:23 -0500

    forms styling in progress

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <BrowserRouter>
-     <h1 className="header">==returnTrue</h1>
+     <h1 className="header">return true;</h1>
       <NavBar setAuthenticated={setAuthenticated} icon={faTimes} />
       <div className="content">
       <Switch>
